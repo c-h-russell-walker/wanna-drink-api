bafd46e0cd0f301f6b00d0b2a9023e90640bde7e
c-h-russell-walker <c.russell.walker@gmail.com>
2016-03-15 17:01:45 -0400

    Prefix all routes with API

--- app/views.py
+++ app/views.py
@@ -7,14 +7,15 @@ from exceptions import InvalidUsage
 from services import create_account
 from models import WannaUser
 
+# TODO - figure out if there's a way to not have to prefix all routes with 'api'
 
-@app.route('/account', methods=['GET'])
+@app.route('/api/account', methods=['GET'])
 def account():
     # Bundled with sign in most likely
     pass
 
 
-@app.route('/account', methods=['POST'])
+@app.route('/api/account', methods=['POST'])
 def post_to_account():
     try:
         # TODO - figure out static method for Flask model
@@ -26,7 +27,7 @@ def post_to_account():
         raise InvalidUsage('Your user data is incorrect', status_code=400)
 
 
-@app.route('/styles', methods=['GET'])
+@app.route('/api/styles', methods=['GET'])
 def get_beer_styles():
     with open('fixtures/beer_styles.json') as data_file:
         beer_styles = json.load(data_file)
