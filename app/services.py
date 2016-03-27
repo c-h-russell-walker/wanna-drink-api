d64530b3c4048eb21f8c67e0c93949b282cc4d81
MALLORY COPPENRATH <MALCOPPENRATH@GMAIL.COM>
2016-03-27 11:37:21 -0400

    Store new users in JSON file

--- app/services.py
+++ app/services.py
@@ -1,3 +1,10 @@
+import json
 
-def create_account():
-    raise NotImplementedError
+def create_account(user):
+    with open('fixtures/users.json', 'r') as file:
+        users = json.loads(file.read())
+
+    users.append(user.to_dict())
+
+    with open('fixtures/users.json', 'w') as file:
+        file.write(json.dumps(users, indent=4, separators=(',', ': ')))
