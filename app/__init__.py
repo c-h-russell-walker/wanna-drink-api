685a76e2a029dc8c1e419ea2d1e963de3c1e083b
c-h-russell-walker <c.russell.walker@gmail.com>
2016-04-09 15:21:40 -0400

    Initial setup with config object and session secret key

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from flask import Flask, jsonify
 from exceptions import InvalidUsage
 
 app = Flask(__name__)
+# TODO - update in order to use dynamic Env. settings (prod, etc) when the time comes
+app.config.from_object('config.common')
 
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
+
