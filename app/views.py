6677574575a4a2f5ceaaceccb8ca9944024d9267
c-h-russell-walker <c.russell.walker@gmail.com>
2016-04-09 14:44:09 -0400

    Return validation err to FE if uname email regex fails

--- app/views.py
+++ app/views.py
@@ -23,8 +23,8 @@ def post_to_account():
         user = WannaUser(request.form)
         # TODO - check if we have one - create it or update
         create_account(user)
-    except Exception as e:
-        raise InvalidUsage('Your user data is incorrect', status_code=400)
+    except Exception as exc:
+        raise InvalidUsage(exc.message, status_code=400)
 
 
 @app.route('/api/styles', methods=['GET'])
