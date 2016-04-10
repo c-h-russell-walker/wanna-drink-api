fdc00d80daf6d64999c427a932dfcf6661c5bf75
c-h-russell-walker <c.russell.walker@gmail.com>
2016-04-10 19:47:16 -0400

    Set session cookie to be able to be read by JS in FE

--- config/common.py
+++ config/common.py
@@ -2,3 +2,8 @@ DEBUG = True
 
 # Secret key for client side session encryption.
 SECRET_KEY = '\xcdo\xdf\x01F\xb9\xb7\xc2R\xaa\x1b\xc8K\x02\xcc\xd2\xf5\x81\x83\xa96\x02\x15^'
+
+# TODO - Check this is the way we'd like to proceed
+# May want to look into `SESSION_COOKIE_DOMAIN` setting
+SESSION_COOKIE_HTTPONLY = False
+
